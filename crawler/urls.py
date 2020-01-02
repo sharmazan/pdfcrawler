651cf498d8096235f8b07c0ad1605c44a00e3bab
Sergii Sharmazanov <sergey.sharm@gmail.com>
2020-01-03 00:06:08 +0200

    Storing PDFs and URLs in database

--- crawler/urls.py
+++ crawler/urls.py
@@ -6,6 +6,7 @@ from crawler import views
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^files/$', views.files, name='files'),
+    url(r'^files/(?P<id>[0-9]+)$', views.file_info, name='file_info'),
     url(r'^urls/$', views.urls, name='urls'),
     url(r'^success/$', views.success, name='success'),
     url(r'^error/$', views.error, name='error'),
--- crawler/views.py
+++ crawler/views.py
@@ -1,49 +1,83 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-import PyPDF2 
+import PyPDF2
+from urllib import urlopen
+# from urllib.request import urlopen
 
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.shortcuts import render, get_object_or_404
+from django.db.models import Count
+from django.views.decorators.csrf import csrf_exempt
 
 from .forms import UploadFileForm
+from .models import PDF, URL
 
 # Create your views here.
 
 def handle_uploaded_file(f):
-    print("handle_uploaded_file")
+    print("handle_uploaded_file, file name: %s" % f.name)
     # PDFFile = open('pdf.pdf','rb')
 
     try:
-        PDF = PyPDF2.PdfFileReader(f)
+        pdf_file = PyPDF2.PdfFileReader(f)
     except PyPDF2.utils.PdfReadError:
         print("handle_uploaded_file can't read PDF file")
         return 1
 
-    pages = PDF.getNumPages()
+    file, created = PDF.objects.get_or_create(filename = f.name)
+    pages = pdf_file.getNumPages()
     key = '/Annots'
     uri = '/URI'
     ank = '/A'
+    # Let's use this set to ensure that we handle the URL only once
+    urls = set()
 
     for page in range(pages):
 
-        pageSliced = PDF.getPage(page)
+        pageSliced = pdf_file.getPage(page)
         pageObject = pageSliced.getObject()
 
         if pageObject.has_key(key):
             ann = pageObject[key]
             for a in ann:
                 u = a.getObject()
-                if u[ank].has_key(uri):
+                if u.has_key(ank) and u[ank].has_key(uri):
                     print u[ank][uri]
+                    if u[ank][uri] not in urls:
+                        # We will handle the URL only once in this PDF file
+                        urls.add(u[ank][uri])
+                        # Check that URL is alive
+                        # To siplicity assume that alive means that URL returns HTTP code 200                        
+                        alive = True if urlopen(u[ank][uri]).getcode() == 200 else False
+                        if alive:
+                            print("Alive!")
+                            # print(urlopen(u[ank][uri]).getcode())
+                        url, new_url = URL.objects.get_or_create(uri = u[ank][uri], defaults={'alive': alive})
+
+                        print("Adding %s url to the file" % url.uri)
+                        file.urls.add(url)
+
+                        if not new_url:
+                            # Update alive status for existing URL
+                            url.alive = alive
+                            url.save()
+
     return 0
 
 
 
 def files(request):
-    return HttpResponse('Files')
+    pdfs = list(PDF.objects.annotate(num_urls=Count('urls')).values())
+    return JsonResponse(pdfs, safe=False)
+
+def file_info(request, id):
+    pdf = get_object_or_404(PDF, id=id)
+    pdf_urls = list(pdf.urls.values())
+    return JsonResponse(pdf_urls, safe=False)    
 
 def urls(request):
-    return HttpResponse('URLs')
+    all_urls = list(URL.objects.annotate(num_files=Count('pdfs')).values())
+    return JsonResponse(all_urls, safe=False)
 
 def success(request):
     return HttpResponse('success')
@@ -51,6 +85,7 @@ def success(request):
 def error(request):
     return HttpResponse('error')
 
+@csrf_exempt
 def home(request):
     if request.method == 'POST':
         print("POST @ home")
